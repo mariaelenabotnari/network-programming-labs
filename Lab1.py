36c26b382e376b95e358fac02b60a8612400e4d2
mariaelenabotnari <152028668+mariaelenabotnari@users.noreply.github.com>
2025-10-04 11:38:49 +0300

    feat: serve requested files and handle missing files with 404

--- Lab1.py
+++ Lab1.py
@@ -1,3 +1,4 @@
+import os.path
 from http.server import BaseHTTPRequestHandler, HTTPServer
 import time
 
@@ -6,15 +7,28 @@ port = 8080
 
 class Server(BaseHTTPRequestHandler):
     def do_GET(self):
-        self.send_response(200)
-        self.send_header("Content-type", "text/html")
-        self.end_headers()
+        file_path = self.path[1:]
+        if self.path == '/':
+            pass
+        if os.path.exists(file_path) and os.path.isfile(file_path):
+            self.send_response(200)
+            if file_path.endswith(".html"):
+                self.send_header('Content-type', 'text/html')
+            elif file_path.endswith(".png"):
+                self.send_header('Content-Type', 'image/png')
+            elif file_path.endswith('.pdf'):
+                self.send_header('Content-type', 'text/pdf')
+            else:
+                self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            with open(file_path, "rb") as file:
+                self.wfile.write(file.read())
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/html")
+            self.end_headers()
+            self.wfile.write(bytes("<html><body><h1>404 - File Not Found</h1></body></html>", "utf-8"))
 
-        self.wfile.write(bytes("<html>", "utf-8"))
-        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
-        self.wfile.write(bytes("<body>", "utf-8"))
-        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
-        self.wfile.write(bytes("</body></html>", "utf-8"))
 
 if __name__ == "__main__":
     web_server = HTTPServer((hostname, port), Server)
